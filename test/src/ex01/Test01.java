36eeb054d4be0b8fe9f83c0d51ec107b14bc13b6
GGG <GGG@DESKTOP-8N3GG4O>
2025-03-06 17:44:35 +0900

    2025 03 06 bnk-green

--- test/src/ex01/Test01.java
+++ test/src/ex01/Test01.java
@@ -3,25 +3,64 @@ package ex01;
 public class Test01 {
 
 	public static void main(String[] args) {
-		
-		//출력문
-		System.out.println("Hello Java!");//println은 다음줄에 띄워쓰기
-		System.out.print("Hello Java!");//print는 다음줄 띄워쓰기 X
-		//자바에서는 문자 ' ', 문자열 " "
-		// 자료형(형태datetype) 변수(이름)  = (값)
-		// 정수형 int
-		// 실수형 float ex)10.3f
-		// 실수형 double
-		// 문자형 char ex) 'A' , "Amm"
-		// 문자열 Stirng은 클래스로 만들어짐
-		
-		
-		
-		int a = 1;
-		int b = 2;
-		int c = a+b;
-		
-		System.out.println(c);
+		/*
+		 * // 출력문 System.out.println("Hello Java!");// println은 다음줄에 띄워쓰기
+		 * System.out.print("Hello Java!");// print는 다음줄 띄워쓰기 X
+		 * 
+		 * 
+		 * 자바에서는 문자 ' ', 문자열 " " 자료형(형태)(datetype) 변수(이름) = (값) 정수형 int 실수형 float
+		 * ex)10.3f 실수형 double 문자형 char ex) 'A' , "Amm" 문자열
+		 * 
+		 * 
+		 * int a = 1; int b = 2; int c = a + b;
+		 * 
+		 * System.out.println(c);
+		 * 
+		 * 이 수가 짝수 홀수 판단 if (c % 2 == 0) { System.out.println("짝수입니다");
+		 * 
+		 * } else { System.out.println("홀수입니다"); }
+		 * 
+		 * 두 변수 중에 더 큰 값 찾기 int d, e;
+		 * 
+		 * d = 2; e = 3;
+		 * 
+		 * if (d > e) { System.out.println("d가 더 큽니다.");
+		 * 
+		 * }
+		 * 
+		 * else if (d == e) { System.out.println("d와 e는 같습니다."); } else {
+		 * System.out.println("e가 더 큽니다."); }
+		 * 
+		 * 3개의 변수를 최대값구하기(조건 같은수는 없음) if (a > b) { if (a > c) {
+		 * System.out.println("a가 더 큽니다."); } else System.out.println("c가 더 큽니다."); }
+		 * 
+		 * else if (b > c) { System.out.println("b가 더 큽니다."); } else
+		 * System.out.println("c가 더 큽니다.");
+		 * 
+		 * 
+		 * 1~10까지 누적하는 프로그램 int f = 0;
+		 * 
+		 * if (f == 0) f = f + 1; if (f == 1) f = f + 1; if (f == 2) f = f + 1; if (f ==
+		 * 3) f = f + 1; if (f == 4) f = f + 1; if (f == 5) f = f + 1; if (f == 6) f = f
+		 * + 1; if (f == 7) f = f + 1; if (f == 8) f = f + 1; if (f == 9) f = f + 1; if
+		 * (f == 10)
+		 * 
+		 * System.out.println(f);
+		 * 
+		 * 
+		 * 반복문 System.out.println(1==1);
+		 * 
+		 * 
+		 * while (1==1) { //무조건 반복 System.out.println(1==1); }
+		 * 
+		 */
+
+		int n = 1;
+		while (n <= 5) {
+			System.out.println("Hello");
+			n = n + 1;
+		}
+
 	}
 
 }
