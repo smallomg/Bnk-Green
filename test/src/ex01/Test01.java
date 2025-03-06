9d4b8df96523492fa454078fd596d34a3b13e243
GGG <GGG@DESKTOP-8N3GG4O>
2025-03-06 15:57:42 +0900

    test1

---
+++ test/src/ex01/Test01.java
@@ -0,0 +1,27 @@
+package ex01;
+
+public class Test01 {
+
+	public static void main(String[] args) {
+		
+		//출력문
+		System.out.println("Hello Java!");//println은 다음줄에 띄워쓰기
+		System.out.print("Hello Java!");//print는 다음줄 띄워쓰기 X
+		//자바에서는 문자 ' ', 문자열 " "
+		// 자료형(형태datetype) 변수(이름)  = (값)
+		// 정수형 int
+		// 실수형 float ex)10.3f
+		// 실수형 double
+		// 문자형 char ex) 'A' , "Amm"
+		// 문자열 Stirng은 클래스로 만들어짐
+		
+		
+		
+		int a = 1;
+		int b = 2;
+		int c = a+b;
+		
+		System.out.println(c);
+	}
+
+}
