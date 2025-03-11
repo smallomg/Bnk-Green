7a52bd976678cb22121b6d1a7a1dd3ea127ca7f2
GGG <GGG@DESKTOP-8N3GG4O>
2025-03-11 18:44:13 +0900

    2025 03 11 백준 1547번

--- test/src/ex01/Baekjoon1547.java
+++ test/src/ex01/Baekjoon1547.java
@@ -7,17 +7,20 @@ public class Baekjoon1547 {
 	public static void main(String[] args) {
 
 		int A[] = { 1, 2, 3 };
-		int B[] = new int[3];
+		int temp = 0;
+
 		Scanner sc = new Scanner(System.in);
 		int M = sc.nextInt();
+
 		for (int i = 0; i < M; i++) {
-			int X = sc.nextInt();
-			int Y = sc.nextInt();
+			int X = sc.nextInt() - 1;
+			int Y = sc.nextInt() - 1;
+
+			temp = A[X];
 			A[X] = A[Y];
-			A[Y] = A[X];
+			A[Y] = temp;
 
 		}
-
 		System.out.print(A[0]);
 
 	}
