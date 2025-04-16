d587d324de823e3170c0c2226099995c3178119a
smallomg <ldy4081@naver.com>
2025-04-16 17:55:11 +0900

    2025 04 16 JDBC JAVA
    JDBC JAVA

---
+++ test/src/jdbc/IteratorTest.java
@@ -0,0 +1,30 @@
+package jdbc;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Iterator;
+import java.util.List;
+
+public class IteratorTest {
+
+	public static void main(String[] args) {
+
+		List<String> list = Arrays.asList("aaa", "bbb", "ccc");
+
+		for (int i = 0; i < list.size(); i++) {
+			System.out.print(list.get(i) + "\t");
+		}
+
+		System.out.println("");
+		for (String s : list) {
+			System.out.print(s + "\t");
+		}
+
+		System.out.println("");
+		Iterator<String> itr = list.iterator();
+		while (itr.hasNext()) {
+			System.out.print(itr.next() + "\t");
+		}
+	}
+
+}
