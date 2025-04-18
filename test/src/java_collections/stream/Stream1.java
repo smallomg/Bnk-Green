7613fb07953cfce145d48d5cfe628ddc5cba13e1
smallomg <ldy4081@naver.com>
2025-04-18 17:54:44 +0900

    2025 04 18
    2025 0418 공부할 내용

--- test/src/java_collections/stream/Stream1.java
+++ test/src/java_collections/stream/Stream1.java
@@ -4,7 +4,18 @@ import java.util.Arrays;
 import java.util.stream.IntStream;
 
 public class Stream1 {
-
+	/*
+	 * Stream 자료의흐름 데이터뭉치(콜렉션 자료)
+	 * 
+	 * -중간연산 filter : 조건에 맞는 자료들 추출하기 map : 자료들 특정한 형태로 변환하기(예:문자열 -> 문자열의 길이)
+	 * ===============================================무조건 알아야함 sorted() : 정렬
+	 * distict() : 중복제거 limit() : 스트림 범위 제한 skip() : 스트림 내 데이터들 중 앞에서 n개 요소 건너뜀
+	 * 
+	 * --최종연산 collect() : 콜렉션 자료 만들어 반환 forEach() : 각 요소에 대한 작업 수행 count() : 요소 개수
+	 * 반환 reduce() : 요소들을 하나의 결과로 만들기 anyMatch(), allMatch() : 조건 검사
+	 * 
+	 * --삼항 연산자 (if -else) 조건 ? 참:거짓
+	 */
 	public static void main(String[] args) {
 
 		// Stream 연산은 기존자료를 변경하지 않는다.
